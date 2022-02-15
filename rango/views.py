3c90d4de5dd50b1f15d1bb9b1ade793b0bad7aae
yangsyz <2627664y@student.gla.ac.uk>
2022-02-15 23:20:42 +0000

    chapter4 is finished

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html = "Rango says hey there partner!" + '<a href="/rango/about/">About</a>'
-    return HttpResponse(html)
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    html = "Rango says here is the about page." + '<a href="/rango/">Index</a>'
-    return HttpResponse(html)
+    return render(request, 'rango/about.html')
 # Create your views here.
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,12 +59,13 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -119,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
