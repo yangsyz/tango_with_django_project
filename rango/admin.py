e6c2dc14c113cde58d7e376ac15051f07dbdfaf4
yangsyz <2627664y@student.gla.ac.uk>
2022-02-17 00:34:00 +0000

    chapter5 is finished

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+#class CategoryInline(admin.StackedInline):
+#    model = Category
+#    extra = 3
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['category']}),
+        (None, {'fields': ['title']}),
+        (None, {'fields': ['url']}),
+        (None, {'fields': ['views']}),
+    ]
+    #inlines = [CategoryInline]
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
